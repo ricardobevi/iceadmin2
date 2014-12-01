0eca4d33fc588535c4766134dfbd3cd8e03dd8a9
ric <ric@priscila>
2014-11-30 23:35:16 -0300

    muevo el boostrap y primeras pruebas para la interfaz

---
+++ application/views/templates/footer.php
@@ -0,0 +1,12 @@
+<?php $this->load->helper('url');?>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="<?php echo base_url("public/js/bootstrap.min.js");?>"></script>
+  </body>
+</html>
