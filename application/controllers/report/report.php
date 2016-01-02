dcd6d86dd9d67ad0d4711381a8e3b88346e21a5b
Ricardo Bevilacqua <ricardob.redmond@gmail.com>
2016-01-02 14:02:32 -0300

    Cambios en el pass del wifi

--- application/controllers/report/report.php
+++ application/controllers/report/report.php
@@ -8,12 +8,16 @@ class Report extends CI_Controller {
     public function __construct() {
 
         parent::__construct ();
+        
+        $this->load->model ( 'report/report_model' );
 
     }
 
     public function index() {
 
-        $this->load->view ( 'report/report_view');
+		$data ['report_data'] = $this->report_model->obtain_daily_sales("2015-12-07");
+		
+        $this->load->view ( 'report/report_view', $data );
 
     }
 
