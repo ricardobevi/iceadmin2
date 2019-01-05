c1d708bbe5beebc4946f53e3422c54e887f89822
Ricardo Bevilacqua <ricardo.bevilacqua@truenorth.co>
2019-01-05 17:50:40 -0300

    Cambia reporte para funcionar con nuevo esquema

--- application/models/report/report_model.php
+++ application/models/report/report_model.php
@@ -11,25 +11,24 @@ class Report_Model extends CI_Model {
 
 
     public function query_report($date){
-
-    $sql  = "SELECT P.label AS product_name, SUM(PT.quantity) AS quantity, SUM(PT.quantity) * Pr.price AS price\n"
-    . "FROM \n"
-    . "	ticket AS T \n"
-    . "    JOIN product_ticket AS PT ON T.id = PT.ticket_id\n"
-    . "    JOIN product AS P ON PT.product_id = P.id\n"
-    . "    JOIN product_price AS PP ON P.id = PP.product_id AND PP.subsidiary_id = 1\n"
-    . "    JOIN price AS Pr ON PP.price_id = Pr.id\n"
-    . "WHERE \n"
-    . "	T.date_time BETWEEN '".$date." 06:00:00' AND '".$date." 06:00:00' + INTERVAL 23 HOUR + INTERVAL 59 MINUTE + INTERVAL 59 SECOND\n"
-    . "    \n"
-    . "GROUP BY P.label \n"
-    . "ORDER BY P.label; ";
-
-	$query = $this->db->query($sql);
-	
-	$result = $query->result_array ();
-	
+    
+        $sql = "SELECT P.label AS product_name, SUM(PT.quantity) AS quantity, (SUM(PT.quantity) * PT.price) AS price
+                FROM 
+                	ticket AS T 
+                    JOIN product_ticket AS PT ON T.id = PT.ticket_id
+                    JOIN product AS P ON PT.product_id = P.id
+                WHERE 
+                	T.date_time BETWEEN '".$date." 06:00:00' AND '".$date." 06:00:00' + INTERVAL 23 HOUR + INTERVAL 59 MINUTE + INTERVAL 59 SECOND
+                    
+                GROUP BY P.label, PT.price
+                ORDER BY P.label";
+    
+    	$query = $this->db->query($sql);
+    	
+    	$result = $query->result_array ();
+    	
         return $result;
+    
     }
 
 }
